11b5545f16b852a8d9e5399e7b834e533640064a
niakale <diakitetenin99@gmail.com>
2025-04-08 17:30:06 +0000

    Nouvelle modifications

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ if(btnToIndex){
         window.location.href= "index.html";
     } );
 }
+
+const color= document.getElementById("color");
+color.addEventListener("change", ()=>{
+  setMainColor(color.value);
+})
 function animateTextByWord(id) {
   const textEl = document.getElementById(id);
   const text = textEl.textContent;
@@ -30,3 +35,43 @@ window.addEventListener("DOMContentLoaded", () => {
   animateTextByWord("animatedText");
   animateTextByWord("animateText");
 });
+function setMainColor(color){
+  
+  document.getElementById("header").style.backgroundColor =color;
+  document.getElementById("education").style.backgroundColor =color;
+  document.getElementById("ico").style.backgroundColor =color;
+  document.getElementById("wom").style.backgroundColor =color;
+  document.getElementById("lan").style.backgroundColor =color;
+  document.getElementById("lois").style.backgroundColor =color;
+  document.getElementById("cont").style.backgroundColor =color;
+  document.getElementById("pro").style.backgroundColor =color;
+  document.getElementById("pro1").style.backgroundColor =color;
+  document.getElementById("pro2").style.backgroundColor =color;
+  document.getElementById("pro3").style.backgroundColor =color;
+  document.getElementById("pro4").style.backgroundColor =color;
+  
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  const bouton = document.getElementById("btnTelecharger");
+
+  bouton.addEventListener("click", function () {
+    const element = document.getElementById("cv");
+
+    const options = {
+      margin: 0,
+      filename: 'CV_Niakalé.pdf',
+      image: { type: 'png, jpg', quality: 1 },
+      html2canvas: { 
+        scale: 2,
+        scrollY:0 
+
+      },
+      jsPDF: { unit: 'px', format: [390,950], orientation: 'portrait' }
+    };
+
+    html2pdf().set(options).from(element).save();
+  });
+});
+
+
